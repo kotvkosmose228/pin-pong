1071e1fb23ea159bf6ff86819a9963b732c9139d
kotvkosmose228 <kotvkosmose226@mail.ru>
2025-01-15 20:17:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ background = transform.scale(image.load("enigma.jpg"),(700,500))
 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, s_image, s_speed, s_x, s_y,s_weight=50,s_height=100):
+    def __init__(self, s_image, s_speed, s_x, s_y,s_weight=50,s_height=150):
         super().__init__()
         self.image = transform.scale(image.load(s_image),(s_weight,s_height))
-        self.speed = s_speed
-        self.rect = self.image.get_rect()
+        self.speed_x = s_speed
+        self.speed_y = s_speed
+        self.rect = Rect(s_x,s_y,s_weight-30,s_height-70)
         self.rect.x = s_x
         self.rect.y = s_y
     def reset(self):
@@ -27,8 +28,22 @@ class GameSprite(sprite.Sprite):
 
 
 
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y -= self.speed_y
+        if self.rect.y < 0:
+            self.speed_y *= -1
+        if self.rect.y > 400:
+            self.speed_y *= -1
+        if self.rect.colliderect(player1.rect):
+            self.speed_x *= -1
+        if self.rect.colliderect(player2.rect):
+            self.speed_x *= -1
+        
 
-
+        
+        
 
 
 
@@ -39,23 +54,25 @@ class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
         if keys [K_w] and self.rect.y > .5:
-            self.rect.y -= self.speed
+            self.rect.y -= self.speed_y
         if keys [K_s]:
-            self.rect.y += self.speed
+            self.rect.y += self.speed_y
 
     def update_r(self):
         keys = key.get_pressed()
         if keys [K_UP] and self.rect.y > .5:
-            self.rect.y -= self.speed
+            self.rect.y -= self.speed_y
         if keys [K_DOWN]:
-            self.rect.y += self.speed
+            self.rect.y += self.speed_y
 
 
 
-player1 = Player('black.png',10,0,250)
+player1 = Player('black.png',5,0,250)
 
+miacik = Ball('ball.png',3,350,250,100,100)
 
-player2 = Player('black.png',10,640,250)
+player2 = Player('black.png',5,640,250)
+
 
 
 
@@ -80,7 +97,8 @@ while play:
     player1.reset()
     player2.update_r()
     player2.reset()
-
+    miacik.update()
+    miacik.reset()
 
     clock.tick(60)
     display.update()
